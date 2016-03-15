ce5ec253df7cd2de79fc1e5423f939fd4ad1a90d
CyanStone <shipeiqing_1991@163.com>
2016-03-15 20:55:34 +0800

    自定义View -ImageTextView

--- app/src/main/java/com/my/cyanstone/himalayan/ui/view/ImageTextView.java
+++ app/src/main/java/com/my/cyanstone/himalayan/ui/view/ImageTextView.java
@@ -2,9 +2,12 @@ package com.my.cyanstone.himalayan.ui.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.my.cyanstone.himalayan.R;
 
 /**
  * Created by 95199 on 2016/3/15.
@@ -13,8 +16,16 @@ public class ImageTextView extends LinearLayout {
 
     private ImageView mImageVIew;
     private TextView mTextView;
+
     public ImageTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        LayoutInflater.from(context).inflate(R.layout.view_text_image,this);
+        init();
+    }
+
+    private void init() {
+        mImageVIew = (ImageView) findViewById(R.id.view_image);
+        mTextView = (TextView) findViewById(R.id.view_text);
     }
 
     public ImageTextView(Context context) {
